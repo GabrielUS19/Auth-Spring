4815c572f5b61123026773cd8d33a54f81051772
Gabriel Uchoa <gabrieluchoa2607@gmail.com>
2026-03-28 20:29:51 -0300

    fix: Fix the User and RefreshToken repositories annotations

--- src/main/java/com/gabriel/authspring/repositories/RefreshTokenRepository.java
+++ src/main/java/com/gabriel/authspring/repositories/RefreshTokenRepository.java
@@ -2,8 +2,10 @@ package com.gabriel.authspring.repositories;
 
 import com.gabriel.authspring.entities.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID> {
 }
--- src/main/java/com/gabriel/authspring/repositories/UserRepository.java
+++ src/main/java/com/gabriel/authspring/repositories/UserRepository.java
@@ -2,10 +2,12 @@ package com.gabriel.authspring.repositories;
 
 import com.gabriel.authspring.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
-
-    User findUserByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
