6ffd64391c3ca37e673cc86d7f6e7dd9dbb43764
Gabriel Uchoa <gabrieluchoa2607@gmail.com>
2026-03-28 08:55:22 -0300

    feat: Implements the repositories of the User and RefreshToken

---
+++ src/main/java/com/gabriel/authspring/repositories/RefreshTokenRepository.java
@@ -0,0 +1,9 @@
+package com.gabriel.authspring.repositories;
+
+import com.gabriel.authspring.entities.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID> {
+}
--- src/main/java/com/gabriel/authspring/repositories/UserRepository.java
+++ src/main/java/com/gabriel/authspring/repositories/UserRepository.java
@@ -1,4 +1,11 @@
 package com.gabriel.authspring.repositories;
 
-public interface UserRepository {
+import com.gabriel.authspring.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    User findUserByEmail(String email);
 }
