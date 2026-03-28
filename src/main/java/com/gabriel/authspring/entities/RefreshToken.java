ec356e8aa3ab920d6253feb6152b64ded2999b7e
Gabriel Uchoa <gabrieluchoa2607@gmail.com>
2026-03-28 20:11:47 -0300

    fix: fix the User and RefreshToken entities annotations

--- src/main/java/com/gabriel/authspring/entities/RefreshToken.java
+++ src/main/java/com/gabriel/authspring/entities/RefreshToken.java
@@ -1,40 +1,45 @@
 package com.gabriel.authspring.entities;
 
 import jakarta.persistence.*;
-import lombok.Data;
-import org.hibernate.annotations.ColumnDefault;
+import lombok.*;
+import org.hibernate.annotations.*;
 import org.hibernate.annotations.Generated;
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
 import org.hibernate.generator.EventType;
 
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
-@Data
-@Entity(name = "refresh_tokens")
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "refresh_tokens")
 public class RefreshToken {
+
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
+    @EqualsAndHashCode.Include
     private UUID id;
 
     @Column(nullable = false, unique = true)
     private String token;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "user_id", nullable = false)
     @OnDelete(action = OnDeleteAction.CASCADE)
     private User user;
 
-    @Column(nullable = false, columnDefinition = "BOOLEAN DEFAULT false")
-    private boolean revoked;
+    @Builder.Default
+    @Column(nullable = false)
+    private boolean revoked = false;
 
     @Column(name = "expires_at", nullable = false)
     private OffsetDateTime expiresAt;
 
-    @Column(name = "created_at", nullable = false)
-    @ColumnDefault("CURRENT_TIMESTAMP")
-    @Generated(event = EventType.INSERT)
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
     private OffsetDateTime createdAt;
-
 }
--- src/main/java/com/gabriel/authspring/entities/User.java
+++ src/main/java/com/gabriel/authspring/entities/User.java
@@ -2,8 +2,9 @@ package com.gabriel.authspring.entities;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.*;
 import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.Generated;
 import org.hibernate.generator.EventType;
 
@@ -12,13 +13,19 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
 
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 @Table(name = "users")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
+    @EqualsAndHashCode.Include
     private UUID id;
 
     @Column(nullable = false)
@@ -33,9 +40,8 @@ public class User {
     @Column(nullable = false)
     private String role;
 
+    @CreationTimestamp
     @Column(name = "created_at", nullable = false, updatable = false)
-    @ColumnDefault("CURRENT_TIMESTAMP")
-    @Generated(event = EventType.INSERT)
     private OffsetDateTime createdAt;
 
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
