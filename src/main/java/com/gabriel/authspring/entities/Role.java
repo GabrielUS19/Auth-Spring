e5f7e65fb0212d58230d51c35ce4881c46e5bd62
Gabriel Uchoa <167583760+GabrielUS19@users.noreply.github.com>
2026-03-30 21:28:25 -0300

    refactor: Refactor the role column in the User entity (#2)

---
+++ src/main/java/com/gabriel/authspring/entities/Role.java
@@ -0,0 +1,20 @@
+package com.gabriel.authspring.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+}
